fafb9bebe79302e040a299ebac859e2fa4a00507
Ragab M. Ammar <ragab.m.ammar@gmail.com>
2020-07-02 21:22:32 +0200

    Add MCamara Locallization

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,20 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
+    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+    Route::get('/', function () {
+        return View::make('welcome');
+    });
+
+    Route::get('test', function () {
+        return 'Test Route';
+    });
 });
