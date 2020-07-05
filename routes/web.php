4250ce6644a2b907e76e7da83bab082e8115c8db
Ragab M. Ammar <ragab.m.ammar@gmail.com>
2020-07-06 00:31:56 +0200

    Themeing Admin Panel

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('front.home');
 });
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
 
 // Route::group(['prefix' => LaravelLocalization::setLocale(),
 //     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
