e5676341bf772b5dd52ce632f1c436d93eb414b0
Ragab M. Ammar <ragab.m.ammar@gmail.com>
2020-07-05 10:31:03 +0200

    Default Auth Frontend Scaffolding

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::group(['prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
-    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/', function () {
-        return View::make('welcome');
-    });
-
-    Route::get('test', function () {
-        return 'Test Route';
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
+
+// Route::group(['prefix' => LaravelLocalization::setLocale(),
+//     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
+//     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+//     Route::get('/', function () {
+//         return View::make('welcome');
+//     });
+
+//     Route::get('test', function () {
+//         return 'Test Route';
+//     });
+// });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
